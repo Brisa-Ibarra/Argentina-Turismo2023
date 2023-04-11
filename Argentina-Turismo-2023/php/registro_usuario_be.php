30a49b3afd9f94d4742a246c89741cd348216b42
Brisa-Ibarra <89554486+Brisa-Ibarra@users.noreply.github.com>
2023-04-11 18:44:17 -0300

    Encriptación y evitar que los datos se repitan

--- Argentina-Turismo-2023/php/registro_usuario_be.php
+++ Argentina-Turismo-2023/php/registro_usuario_be.php
@@ -7,8 +7,38 @@
 	$usuario = $_POST['usuario'];
 	$password = $_POST['password'];
 
+	//Encriptamiento de la contraseña
+	$password = hash('sha512', $password);
+
+
 	$query = "INSERT INTO usuarios(nombre_completo, correo, usuario, password) 
 			  VALUES('$nombre_completo', '$correo', '$usuario', '$password')";
+
+	// Verificar que el correo no se repita en la BD 
+	$verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo'");
+
+	if(mysqli_num_rows($verificar_correo) > 0) {
+		echo '
+			<script>
+				alert("Este correo ya está registrado, intenta con otro diferente");
+				window.location = "../login.php"
+			</script>
+		';
+		exit();
+	}
+
+	// Verificar que el nombre de usuario no se repita en la BD 
+	$verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario = '$usuario'");
+
+	if(mysqli_num_rows($verificar_usuario) > 0) {
+		echo '
+			<script>
+				alert("Este usuario ya está registrado, intenta con otro diferente");
+				window.location = "../login.php"
+			</script>
+		';
+		exit();
+	}
 
 
 	$ejecutar = mysqli_query($conexion, $query);
@@ -32,6 +62,4 @@
 	}
 
 	mysqli_close($conexion);
-
-
 ?>
